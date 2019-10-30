ae273021376fd3420787f9d1d55ac8a0d44ee8e7
Arturo Cantú <a01196412@itesm.mx>
2019-10-30 11:28:27 -0600

    Revert "Revert "getWeightbyKeyword""
    This reverts commit 9d7ebf0a05023d85c51e46d47a85b95dda5988aa.

---
+++ semana-i-2019-master/semana-i-2019-master/src/components/my-component/my-component.d.ts
@@ -0,0 +1,8 @@
+import { EventEmitter } from '../../../dist/types/stencil.core';
+export declare class MyComponent {
+    data: any;
+    printConsoleLog(): any;
+    myCustomEvent: EventEmitter;
+    private myEvent;
+    render(): any;
+}
